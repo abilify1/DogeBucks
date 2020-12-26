4720d70468a9335cb6a5bd3427cbefacdd4ecfbf
AbilSeno <48641526+AbilSeno@users.noreply.github.com>
2020-12-26 13:15:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -807,8 +807,8 @@ async function starts() {
                                 case 'igstory':
                                    try {
                                     if (args.length < 1) return reply('usernamenya mana sayang?')
-                                    if (args.length == 2){
-                                     const igs1 = await fetchJson(`https://api.vhtear.com/igstory?query=${encodeURIComponent(body.slice(9))}&apikey=Abil_Seno2k20`)
+                                    if (args.length == 1){
+                                     const igs1 = await fetchJson(`https://api.vhtear.com/igstory?query=${encodeURIComponent(args[1])}&apikey=Abil_Seno2k20`)
                                      if (igs1.result.status == "error message") return reply(`[!] Username salah!!`)
                                      if (igs1.result.itemlist == undefined || igs1.result.itemlist == null) return reply(`[!] Username yang dituju tidak memiliki story`)
                                      igs_cap = `Saya menemukan *${igs1.result.itemlist.length}* story, di username *${igs.result.owner_username}*\n=================\n`
